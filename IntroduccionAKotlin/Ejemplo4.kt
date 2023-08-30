0508d988fda892ca4ba41be91dabc74c7ab7c876
al3pena <131722878+al3pena@users.noreply.github.com>
2023-08-30 20:07:39 -0300

    Create Ejemplo4.kt

---
+++ IntroduccionAKotlin/Ejemplo4.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+fun main(){
+
+    //Operadores matemáticos
+
+    var suma: Int = 10 + 10
+    var resta: Int = 50 - 10
+    var multiplicacion: Int = 10 * 10
+    var division = 10 / 6 //1.66667
+    var resto: Int = 10 % 6
+    var incremento: Int = 0
+    var decremento: Int = 0
+
+
+    println(suma)
+    println(resta)
+    println(multiplicacion)
+    println(division)
+    println(resto)
+    println(++incremento)
+    println(--decremento)
+
+    var x = 10
+    x += 5
+    x = x + 5
+    println(x)
+
+}
