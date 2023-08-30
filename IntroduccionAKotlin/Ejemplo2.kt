280fef65d8bee4b2917ca0300f81c04d6f20687a
al3pena <131722878+al3pena@users.noreply.github.com>
2023-08-30 20:06:50 -0300

    Create Ejemplo2.kt

---
+++ IntroduccionAKotlin/Ejemplo2.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+fun main(){
+
+    //Tipos de datos en Kotlin
+
+    val soyUnString:String = "Hola soy String" //string comilla doble
+    val soyEntero: Int = 50
+    val soyDouble: Double = 3.1415
+    val soyChar: Char = '%' //Los char llevan comilla simple
+    val soyBooleano: Boolean = true
+
+
+    // Números. se dividen en dos grandes grupos: ENTEROS
+    //-----> Byte -128 a 127
+    //-----> Short -32768 al 32767
+    //-----> Int -2.147.483.648
+    //-----> Long -9.223.372.036.854.775.807
+    // Los de COMA FLOTANTE
+    //-----> Float
+    //-----> Double
+
+    val soyUnByte: Byte = 100
+    val soyUnShort: Short = 12000
+    val soyUnInt: Int = 255555555
+    val soyUnLong: Long = 2522225555555
+
+    //Numero COMA FLOTANTE
+
+    val soyUnFloat: Float = 6.75F
+    println(soyUnFloat)
+    6
+    val soyUnDouble: Double = .75
+    println(soyUnDouble)
+    println(soyChar)
+}
