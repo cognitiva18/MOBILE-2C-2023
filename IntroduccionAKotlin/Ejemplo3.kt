b988be74f4102180e39c4458c03f34cbf40d7c9c
al3pena <131722878+al3pena@users.noreply.github.com>
2023-08-30 20:07:15 -0300

    Create Ejemplo3.kt

---
+++ IntroduccionAKotlin/Ejemplo3.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+fun main(){
+
+    // .toShort()
+    // .toString()
+    // .toDouble()
+    // .toInt()
+    // ....
+
+
+    val x: Int = 5
+    val y: Long = x.toLong()
+    val z: String = x.toString()
+
+    println(x)  
+}
